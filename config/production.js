0050b2637d2bfe4d34dcf21613f933b22fa2eed9
ryu <ryuforever@163.com>
2016-03-07 16:00:01 +0800

    domain

--- config/production.js
+++ config/production.js
@@ -1,7 +1,7 @@
 module.exports = {
   mongodb: 'mongodb://myUserAdmin:abc123@localhost:27017/redux_blog',
   mongoAuthDB: 'admin',
-  cookiesDomain: 'http://139.162.59.123',
+  cookiesDomain: '.139.162.59.123',
   cookieExpires: 86400,
   sessionSecret: 'simplereduxinstance'
 };

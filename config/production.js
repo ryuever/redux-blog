87d04b7ceda2133ea68a71ef6f5ad33b2661ade5
ryu <ryuforever@163.com>
2016-03-07 14:27:50 +0800

    Resolve prod cookie issue

---
+++ config/production.js
@@ -0,0 +1,8 @@
+module.exports ={
+  mongodb: 'mongodb://myUserAdmin:abc123@localhost:27017/redux_blog',
+  mongoAuthDB: 'admin',
+  cookiesDomain: 'http://139.162.59.123',
+  cookiesPath: '/',
+  cookieExpires: 86400,
+  sessionSecret: 'simplereduxinstance'
+};

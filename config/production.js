f895ffced0665a15140fb8e09694e9b336f4a26f
ryu <ryuforever@163.com>
2016-03-07 15:10:35 +0800

    update

--- config/production.js
+++ config/production.js
@@ -1,5 +1,5 @@
-module.exports ={
-  mongodb: 'mongodb://myUserAdmin:abc123@http://139.162.59.123:27017/redux_blog',
+module.exports = {
+  mongodb: 'mongodb://myUserAdmin:abc123@localhost:27017/redux_blog',
   mongoAuthDB: 'admin',
   cookiesDomain: 'http://139.162.59.123',
   cookieExpires: 86400,

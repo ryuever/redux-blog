95775ba06ee6bda2a61533e53968389fd7ac50ce
ryu <ryuforever@163.com>
2016-06-12 11:33:36 +0800

    fix production config

--- app/server.jsx
+++ app/server.jsx
@@ -39,7 +39,7 @@ function renderFullPage(renderedContent, initialState, head={
     <link rel="stylesheet" type="text/css" href="/modal.css" />
     <link rel="stylesheet" type="text/css" href="/md.css" />
     <link rel="stylesheet" type="text/css" href="/font-awesome-4.6.3/css/font-awesome.min.css" />
-    </body>
+
     </html>
 
   `;
--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -95,11 +95,6 @@ module.exports = [
       // new webpack.optimize.CommonsChunkPlugin("vendor", "vendor.bundle.js"),
       new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.bundle.js', Infinity),
       new ExtractTextPlugin("[name].css"),
-      new webpack.optimize.UglifyJsPlugin({
-        compressor: {
-          warnings: false
-        }
-      }),
       new webpack.DefinePlugin({
         __DEVCLIENT__: false,
         __DEVSERVER__: false,
@@ -137,11 +132,6 @@ module.exports = [
       new webpack.optimize.OccurenceOrderPlugin(),
       // new ExtractTextPlugin("styles/main.css"),
       new ExtractTextPlugin("[name].css"),
-      new webpack.optimize.UglifyJsPlugin({
-        compressor: {
-          warnings: false
-        }
-      }),
       new webpack.DefinePlugin({
         __DEVCLIENT__: false,
         __DEVSERVER__: false,
@@ -150,77 +140,3 @@ module.exports = [
     ],
   }
 ];
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* var path = require('path');
-   var webpack = require('webpack');
-   var ExtractTextPlugin = require("extract-text-webpack-plugin");
-
-   var hotMiddlewareScript = 'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000&reload=true';
-   var assetsPath = path.join(__dirname, '..', 'public', 'assets');
-
-   var config = {
-   devtool: 'cheap-module-eval-source-map',
-   context: path.join(__dirname, '../app'),
-   entry:{
-   app: ['./client', hotMiddlewareScript],
-   vendor: './vendor',
-   style: './stylesheets/rbMain'
-   },
-   output: {
-   path: assetsPath,
-   filename: '[name].js',
-   publicPath: '/assets/'
-   },
-   plugins: [
-   new webpack.optimize.OccurenceOrderPlugin(),
-   new webpack.HotModuleReplacementPlugin(),
-   new webpack.NoErrorsPlugin(),
-   new webpack.DefinePlugin({
-   __TEST__: JSON.stringify(JSON.parse(process.env.TEST_ENV || 'false')),
-   __DEV__: true
-   }),
-   new webpack.optimize.CommonsChunkPlugin("vendor", "vendor.bundle.js"),
-   new ExtractTextPlugin("[name].css")
-   ],
-   module:{
-   loaders: [
-   {
-   test: /\.js$|\.jsx$/,
-   loader: 'babel',
-   include: path.join(__dirname,'..', 'app')
-   },
-   {
-   test: /\.less$/,
-   loader: ExtractTextPlugin.extract("style-loader", "css-loader!less-loader")
-   },
-   { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' },
-   {test: /\.json$/, loader: "json-loader"}
-   ]
-   },
-   resolve: {
-   extensions: ['', '.js', '.jsx', '.scss', '.less'],
-   root: path.resolve(__dirname, '..', 'app'),
-   modulesDirectories: [
-   'node_modules', 'app'
-   ],
-   fallback: path.resolve(__dirname, '..', 'app'),
-   alias: {
-   MakeRequest: path.resolve(__dirname, '..', 'app/util/MakeRequest')
-   }
-   }
-   }
-
-   module.exports = config; */
